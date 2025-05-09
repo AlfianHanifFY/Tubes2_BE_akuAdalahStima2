660f32f5c2267bf82e69e817866e6111b8d8d043
mineraleee <helenigratia@gmail.com>
2025-05-09 20:14:33 +0700

    feat: add function

--- Element/Element.go
+++ Element/Element.go
@@ -59,3 +59,42 @@ func (e Element) LeftChildren() []Element {
 func (e Element) RightChildren() []Element {
 	return GetElements(e.Right)
 }
+
+// Define base components, jika 'time' ga masuk
+var BaseComponents = map[string]bool{
+	"Air":   true,
+	"Earth": true,
+	"Fire":  true,
+	"Water": true,
+}
+
+// Kelompokin berdasarkan rootnya
+func BuildRecipeMap() map[string][]Element {
+	recipeMap := make(map[string][]Element)
+	for _, r := range allElements {
+		recipeMap[r.Root] = append(recipeMap[r.Root], r)
+	}
+	return recipeMap
+}
+
+// Check if an item is a base component
+func IsBaseComponent(item string) bool {
+	return BaseComponents[item]
+}
+
+// Check if all leaf nodes are base components
+func ValidateTree(tree Tree) bool {
+	// If this is a leaf node
+	if len(tree.Children) == 0 {
+		return IsBaseComponent(tree.Root.Root)
+	}
+
+	// Check all children
+	for _, child := range tree.Children {
+		if !ValidateTree(child) {
+			return false
+		}
+	}
+
+	return true
+}
