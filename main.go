b7199fa19aeb70d84494a72e6ab9a4fa202f2d75
AlfianHanifFY <alfian.yustanto@gmail.com>
2025-05-09 10:32:33 +0700

    feat: add main

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	handler "stima-2-be/Handler"
+)
+
+// CORS middleware untuk menambahkan header Access-Control-Allow-Origin
+func enableCORS(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Atau bisa menggunakan "http://localhost:3000" untuk lebih spesifik
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// Jika request method OPTIONS, langsung beri respons OK (pre-flight)
+		if r.Method == http.MethodOptions {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Lanjutkan ke handler asli
+		next.ServeHTTP(w, r)
+	}
+}
+
+func main() {
+	// Menambahkan CORS middleware ke handler
+	http.HandleFunc("/GetElement", enableCORS(handler.GetElementHandler))
+	http.HandleFunc("/scrap", enableCORS(handler.ScrapHandler))
+	http.HandleFunc("/ShortestPath", enableCORS(handler.ShortestPathHandler))
+	http.HandleFunc("/TestTree", enableCORS(handler.TestTreeHandler))
+
+	// Jalankan server pada port 8080
+	fmt.Println("Server is running on http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
